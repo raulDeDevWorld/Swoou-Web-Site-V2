066b34c5e46348862dba50b45c0fc45c4e1c7bc3
raulDeDevWorld <proyection009@gmail.com>
2023-11-24 18:55:08 -0400

    DC

--- src/app/layout.js
+++ src/app/layout.js
@@ -3,10 +3,6 @@ import './globals.css'
 import Navbar from '@/components/Navbar'
 const inter = Inter({ subsets: ['latin'] })
 
-export const metadata = {
-  title: 'Create Next App',
-}
-
 export default function RootLayout({ children }) {
   return (
     <html lang="es">
